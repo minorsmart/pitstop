ea0f0df8f8d116928fe53ce6eaa3bf497f185662
witusj <wfth69@gmail.com>
2018-10-23 17:14:49 +0200

    Updates

---
+++ precache-manifest.888c07da337206bd4ed70c6c83fc9e2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2339b49a047f529004a",
+    "url": "/pitstop/static/js/runtime~main.e2339b49.js"
+  },
+  {
+    "revision": "ee608b466297d572d86f",
+    "url": "/pitstop/static/js/main.ee608b46.chunk.js"
+  },
+  {
+    "revision": "df2ee25a74111c682a73",
+    "url": "/pitstop/static/js/1.df2ee25a.chunk.js"
+  },
+  {
+    "revision": "ee608b466297d572d86f",
+    "url": "/pitstop/static/css/main.1ddb599f.chunk.css"
+  },
+  {
+    "revision": "3d6cba348e4665135639937198e8147d",
+    "url": "/pitstop/index.html"
+  }
+];
