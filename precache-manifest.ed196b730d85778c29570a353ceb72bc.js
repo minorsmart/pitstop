0fb9abf3ecf748b1b461062c1239032e25cab6ae
witusj <wfth69@gmail.com>
2018-10-23 19:37:08 +0200

    Updates

---
+++ precache-manifest.ed196b730d85778c29570a353ceb72bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2339b49a047f529004a",
+    "url": "/pitstop/static/js/runtime~main.e2339b49.js"
+  },
+  {
+    "revision": "1d16b81c7a55e62263d7",
+    "url": "/pitstop/static/js/main.1d16b81c.chunk.js"
+  },
+  {
+    "revision": "605a52dcd78dd5f576ee",
+    "url": "/pitstop/static/js/1.605a52dc.chunk.js"
+  },
+  {
+    "revision": "1d16b81c7a55e62263d7",
+    "url": "/pitstop/static/css/main.7b2e0eb4.chunk.css"
+  },
+  {
+    "revision": "7085d9351db8df585da5389e26b15b48",
+    "url": "/pitstop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/pitstop/precache-manifest.27376bd6bff2c527b9560ef2e8670bba.js"
+  "/pitstop/precache-manifest.ed196b730d85778c29570a353ceb72bc.js"
 );
 
 workbox.clientsClaim();
