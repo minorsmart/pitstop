ded1cdb37f83c7123c796d432325b4dd11c0c953
witusj <wfth69@gmail.com>
2018-10-23 19:11:43 +0200

    Updates

---
+++ precache-manifest.f894905cdecf4f9ee5e7cf42135788a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50950e693904179df204",
+    "url": "/msipitstop/static/js/runtime~main.50950e69.js"
+  },
+  {
+    "revision": "0897493d26feda4d9ff9",
+    "url": "/msipitstop/static/js/main.0897493d.chunk.js"
+  },
+  {
+    "revision": "9a8f7032f9c25269c355",
+    "url": "/msipitstop/static/js/1.9a8f7032.chunk.js"
+  },
+  {
+    "revision": "0897493d26feda4d9ff9",
+    "url": "/msipitstop/static/css/main.727abeb2.chunk.css"
+  },
+  {
+    "revision": "296aa13df2aefce6ebd84a020fa8e91e",
+    "url": "/msipitstop/index.html"
+  }
+];
